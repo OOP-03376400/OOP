612098a754bf16541a53e98c75bd9c63dd47624b
SeanWright1992 <38123765+SeanWright1992@users.noreply.github.com>
2019-11-04 14:38:55 -0800

    Add files via upload

---
+++ OscillateS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3
+{
+    class OscillateS : Iseqs
+    {
+        public OscillateS()
+        {
+
+        }
+
+        public OscillateS(uint PValue) : base(PValue)
+        {
+        }
+    }
+}
